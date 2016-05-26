5fcb9fb3ad5e825b6b17ef11a1c90d356d341a58
xlgwr <xlgwr@163.com>
2016-05-26 09:18:27 +0800

    fix some bug

--- 0GetWebDataExt/getwebData.js
+++ 0GetWebDataExt/getwebData.js
@@ -1,4 +1,3 @@
-
 var currLocal = document.location.href;
 
 var _updateWebURLTo = 'https://www.icris.cr.gov.hk/normal.html';
@@ -17,19 +16,27 @@ var _CBDS_SearchUrl = "https://www.icris.cr.gov.hk/csci/CBDS_Search.do";
 //终审及高等法院,所有裁判法院,勞資審裁處,小额钱债审裁处
 var _1for_judiciaryUrl = "http://www.judiciary.gov.hk/en/crt_lists/daily_caulist.htm";
 //上诉记录,//判案书//判刑理由//裁决理由
-var _2for_legalref = "http://legalref.judiciary.gov.hk/lrs/common/search/search_appeal.jsp?DIS=";//DIS=000001
+var _2for_legalref = "http://legalref.judiciary.gov.hk/lrs/common/search/search_appeal.jsp?DIS="; //DIS=000001
 
-$(function () {
+$(function() {
     //绿色安全
     //http://121.15.207.49:3006/
-    if (currLocal.indexOf("121.15.207.49:3006") > -1) {
+    if (currLocal.indexOf("121.15.207.49:3006") > -1 || currLocal.indexOf("125.88.131.8:3004") > -1) {
         console.clear();
         console.log("绿色安全");
+        var tmpUrl = currLocal.indexOf('125.88.131.8:3004') > -1 ? "125.88.131.8:3004" : "121.15.207.49:3006";
+
         $.ajax({
             type: "POST",
-            url: "http://121.15.207.49:3006/Intercept/GoOnVist",
-            data: { FilterType: "" + $("#hid_filtertype").attr("value") + "", UserId: "" + $("#hid_userid").attr("value") + "", Urlpath: "" + $("#hid_urlpath").attr("value") + "", Logid: "" + $("#hid_logid").attr("value") + "", Password: '88888888' },
-            success: function (data, textStatus) {
+            url: "http://" + tmpUrl + "/Intercept/GoOnVist",
+            data: {
+                FilterType: "" + $("#hid_filtertype").attr("value") + "",
+                UserId: "" + $("#hid_userid").attr("value") + "",
+                Urlpath: "" + $("#hid_urlpath").attr("value") + "",
+                Logid: "" + $("#hid_logid").attr("value") + "",
+                Password: '88888888'
+            },
+            success: function(data, textStatus) {
                 console.log(this.data);
                 if (data[0].code != 0)
                     console.log(data[0].msg)
@@ -38,7 +45,7 @@ $(function () {
                     window.event.returnValue = false;
                 }
             },
-            error: function (e) {
+            error: function(e) {
                 console.log(e)
             }
         });
@@ -90,8 +97,3 @@ $(function () {
     }
     //console.log("no Listen:" + currLocal);
 });
-
-
-
-
-
